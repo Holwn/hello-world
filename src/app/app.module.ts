11ec465d098726ca61f42d35945ea2024ff1c69a
hoangle <hoangcisox@gmail.com>
2023-11-01 13:36:27 +0700

    learn OverView Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentOverviewByCliComponent } from './component-overview-by-cli/component-overview-by-cli.component';
+import { ComponentOverViewByManual } from './component-overview-by-manual/component-overview-by-manual';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentOverviewByCliComponent,
+    ComponentOverViewByManual
   ],
   imports: [
     BrowserModule
---
+++ src/app/component-overview-by-manual/component-overview-by-manual.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-component-overview-by-manual',
+    templateUrl: './component-overview-by-manual.component.html',
+    styleUrls: ['./component-overview-by-manual.component.scss']
+})
+export class ComponentOverViewByManual {
+
+}
